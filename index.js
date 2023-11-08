a0fd1ef3455799e7c3d9bda4e585de48cb0c784a
VK <56174314+vk-56@users.noreply.github.com>
2023-11-08 20:44:26 +0530

    Finished till 3.8

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+// Custom token for data
+morgan.token('res-data', function (response) {
+    return JSON.stringify(response.body)
+})
+
 
 // Middleware
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-data'))
 
 let persons = [
     { 
