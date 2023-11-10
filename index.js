a1eda5f3b3573d0642526b216be7df6287572877
VK <56174314+vk-56@users.noreply.github.com>
2023-11-10 15:27:21 +0530

    Finished till 3.18

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-data'))
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}  
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+} 
 
 let persons = [
 ]
@@ -42,34 +53,39 @@ app.get('/api/persons', (request, response) => {
 })
 
 // Display data info
-app.get('/info', (request, response) => {
-    response.send(`<h2>Phonebook has info for ${persons.length} people</h2><h2>${new Date()}</h2>`)
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({})
+        .then( documentCount => {
+            console.log(documentCount)
+            response.send(`<h2>Phonebook has info for ${documentCount} people</h2><h2>${new Date()}</h2>`)   
+        })
+        .catch(error => next(error))
 })
 
 // Display specific person details
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const requiredPerson = persons.find( (person) => person.id === id)
-    if(requiredPerson) {
-        response.status(200).json(requiredPerson)
-    }
-    else {
-        response.status(400).json({
-            "error" : `Person with id ${id} not found`
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then( returnedPerson => {
+            console.log(returnedPerson)
+            if(returnedPerson) {
+                response.json(returnedPerson)
+            }
+            else {
+                response.status(404).end()
+            }
         })
-    }
+        .catch(error => next(error))
 })
 
 // Delete specific person details
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter( (person) => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then( returnedPerson => {
+            console.log(returnedPerson)
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateId = () => {
-    return persons.length > 0 ? Math.max(...persons.map( (person) => person.id)) + 1 : 0
-}
 
 // Create new person details
 app.post('/api/persons', (request, response) => {
@@ -97,6 +113,27 @@ app.post('/api/persons', (request, response) => {
             response.status(200).json(savedPerson)
         })
 })
+
+// Replace number with new number
+app.put('/api/persons/:id', (request, response, next) => {
+    const personObj = {
+        name: request.body.name,
+        number: request.body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, personObj, { new: true})
+        .then( updatedPerson => {
+            console.log(updatedPerson)
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)    
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
